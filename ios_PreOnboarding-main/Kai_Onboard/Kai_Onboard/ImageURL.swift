b1d487019a217102079b4b79835441b46cc32bcf
KaiKimiOS <110045441+KaiKimiOS@users.noreply.github.com>
2023-02-28 11:35:25 +0900

    Add files via upload
    원티드 프리온보딩 챌린지 iOS 2차과정 사전과제

---
+++ ios_PreOnboarding-main/Kai_Onboard/Kai_Onboard/ImageURL.swift
@@ -0,0 +1,21 @@
+//
+//  ImageURL.swift
+//  Kai_Onboard
+//
+//  Created by kai on 2023/02/28.
+//
+
+import Foundation
+
+public enum imageURL {
+    
+    static let url = URL(string: "https://picsum.photos/200/300")
+    
+}
+
+
+//static let url1 = URL(string: "https://picsum.photos/id/870/200/300?grayscale&blur=2")
+//static let url2 = URL(string: "https://picsum.photos/id/237/200/300")
+//static let url3 = URL(string: "https://picsum.photos/seed/picsum/200/300")
+//static let url4 = URL(string: "https://picsum.photos/200/300")
+//static let url5 = URL(string: "https://picsum.photos/200/300")
---
+++ ios_PreOnboarding-main/Kai_Onboard/Kai_Onboard/PhotoURL.swift
@@ -0,0 +1,7 @@
+//
+//  PhotoURL.swift
+//  Kai_Onboard
+//
+//  Created by kai on 2023/02/27.
+//
+
